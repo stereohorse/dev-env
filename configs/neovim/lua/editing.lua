083788dec886271c2bb2b08bddb03460eb5c6db7
Viet Nguyen <viet@qadojo.tech>
2022-08-23 13:44:30 +0300

    feat: setup keybindings for diagnostics

--- configs/neovim/lua/editing.lua
+++ configs/neovim/lua/editing.lua
@@ -109,3 +109,22 @@ require('Comment').setup()
 -- DIAGNOSTICS
 require('trouble').setup()
 
+vim.api.nvim_set_keymap("n", "<leader>xx", "<cmd>Trouble<cr>",
+  {silent = true, noremap = true}
+)
+vim.api.nvim_set_keymap("n", "<leader>xw", "<cmd>Trouble workspace_diagnostics<cr>",
+  {silent = true, noremap = true}
+)
+vim.api.nvim_set_keymap("n", "<leader>xd", "<cmd>Trouble document_diagnostics<cr>",
+  {silent = true, noremap = true}
+)
+vim.api.nvim_set_keymap("n", "<leader>xl", "<cmd>Trouble loclist<cr>",
+  {silent = true, noremap = true}
+)
+vim.api.nvim_set_keymap("n", "<leader>xq", "<cmd>Trouble quickfix<cr>",
+  {silent = true, noremap = true}
+)
+vim.api.nvim_set_keymap("n", "gR", "<cmd>Trouble lsp_references<cr>",
+  {silent = true, noremap = true}
+)
+
